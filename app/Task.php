513127daaf326c3dba18897ae5371066255325b9
syo-sa1982 <fujii1308@gmail.com>
2017-01-13 07:34:58 +0900

    PhpStorm用の設定

--- app/Task.php
+++ app/Task.php
@@ -5,6 +5,22 @@ namespace App;
 use App\User;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Task
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Task whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Task extends Model
 {
     /**
--- app/User.php
+++ app/User.php
@@ -14,6 +14,21 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
  * App\User
  *
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Task[] $tasks
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
  */
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
