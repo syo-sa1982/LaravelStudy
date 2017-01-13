2bfeb99dc9a53130c6e0b180eba3f10c6bf1a6a9
syo-sa1982 <fujii1308@gmail.com>
2017-01-13 09:16:01 +0900

    タスクの追加と表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -19,5 +21,30 @@ class TaskController extends Controller
         $this->middleware('auth');
     }
 
+    public function index(Request $request)
+    {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
 
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * 新規タスク
+     * @param Request $request
+     * @return \Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
 }
